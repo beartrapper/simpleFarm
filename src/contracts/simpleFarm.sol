f1ae2299a41429810f6f483591e629965b6801e1
Dendolium <dendodendolium@gmail.com>
2022-05-05 21:29:40 +0530

    basic structureof contract added

---
+++ src/contracts/simpleFarm.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Farm {
+    
+    
+    //FUNCTIONS
+    //deposit funds 
+    //withdraw funds 
+    //-> should auto call withdraw yeild
+    //withdraw yeild
+    //calculate a user's yield
+    //add new pool
+    
+
+    //NOTES
+    //yeild would be based on multiple factors
+    //-> time
+    //-> user's contribution
+    //-> alloc points
+    //there has to be a total % of emissions
+    //-> normally its called alloc points
+}
